5038b7c1b65954c713263b6503adcea6f9553bb2
Malcak <malcakx@gmail.com>
2022-01-23 22:17:23 -0500

    03-02-data-types-arrays

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 fn main() {
-    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let a = [1, 2, 3, 4, 5];
 
-    let five_hundred = x.0;
+    let months = [
+        "January", 
+        "February", 
+        "March", 
+        "April", 
+        "May", 
+        "June", 
+        "July", 
+        "August", 
+        "September", 
+        "October", 
+        "November", 
+        "December"
+    ];
 
-    let six_point_four = x.1;
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
 
-    let one = x.2;
+    let a = [3; 5];
 }
