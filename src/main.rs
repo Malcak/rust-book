fddbdf3acff814cd3b6b5a6be1166cce1c626620
Malcak <malcakx@gmail.com>
2022-01-24 21:44:51 -0500

    03-05-summary-generate-nth-fibonacci-number

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use std::io;
+
 fn main() {
-    for number in (1..4).rev() {
-        println!("{}!", number);
+    let mut n = String::new();
+    println!("Please write the nth number: ");
+    io::stdin()
+        .read_line(&mut n)
+        .expect("Failed to read the input");
+    let n: u32 = n.trim().parse().expect("Bad input");
+
+    let mut index: u32 = 0;
+    let mut aux: u32;
+    let mut values: (u32, u32) = (0, 1);
+    while index < n {
+        index += 1;
+        aux = values.0;
+        values.0 = values.1;
+        values.1 = aux + values.1;
+        println!("{}th value is: {}", index, values.0);
     }
-    println!("LIFTOFF!!!");
 }
