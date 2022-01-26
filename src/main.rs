f14a614e79de520f91a6755851d6c4bb333eeb93
Malcak <malcakx@gmail.com>
2022-01-25 22:19:34 -0500

    04-02-allow-multiple-mutable-references-but-not-simultaneous

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
     let mut s = String::from("hello");
 
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    let r3 = &mut s; // BIG PROBLEM
-    // cannot borrow `s` as mutable because it is also borrowed as immutable
-    
-    println!("{}, {}, and {}", r1, r2, r3);
+    {
+        let r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut s;
 }
