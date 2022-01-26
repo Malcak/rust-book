28dcbbfb8e046be9b64233a7ad07bc9cdbfd2308
Malcak <malcakx@gmail.com>
2022-01-25 22:18:38 -0500

    04-02-combining-mutable-and-inmutable-references

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     let mut s = String::from("hello");
 
-    let r1 = &mut s;
-    let r2 = &mut s;
-    // cannot borrow `s` as mutable more than once at a time
-
-    println!("{}, {}", r1, r2);
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM
+    // cannot borrow `s` as mutable because it is also borrowed as immutable
+    
+    println!("{}, {}, and {}", r1, r2, r3);
 }
