09e9c8036c4c43b03b5d6e7540103700412d9c0b
Malcak <malcakx@gmail.com>
2022-01-25 22:25:26 -0500

    04-02-last-usage-immutable-ref-before-mutable-ref

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+// this code will compile because the last usage of the immutable references,
+// the println!, occurs before the mutable reference is introduced
+
 fn main() {
     let mut s = String::from("hello");
 
-    {
-        let r1 = &mut s;
-    } // r1 goes out of scope here, so we can make a new reference with no problems.
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
 
-    let r2 = &mut s;
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
