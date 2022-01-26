36fd146541dafcc72c8bfcd01e90179083c12231
Malcak <malcakx@gmail.com>
2022-01-25 21:28:13 -0500

    04-01-ownership

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-use std::io;
-
 fn main() {
-    let mut n = String::new();
-    println!("Please write the nth number: ");
-    io::stdin()
-        .read_line(&mut n)
-        .expect("Failed to read the input");
-    let n: u32 = n.trim().parse().expect("Bad input");
+    // s1 was invalidated and his value was moved to s2
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}, world!", s1);
 
-    let mut index: u32 = 0;
-    let mut aux: u32;
-    let mut values: (u32, u32) = (0, 1);
-    while index < n {
-        index += 1;
-        aux = values.0;
-        values.0 = values.1;
-        values.1 = aux + values.1;
-        println!("{}th value is: {}", index, values.0);
-    }
+    // this is how deeply copy the heap data
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
