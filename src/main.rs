1bee71f05213dfbaf3d9b8e7dcdae0f69dfd35e4
Malcak <malcakx@gmail.com>
2022-01-25 23:22:37 -0500

    04-03-string-slices

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 fn main() {
     let s = String::from("Los amigos son amigos para siempre y por siempre");
-    let index = first_word(&s);
-    println!("the index of the first word is: {}", index);
+    let word = first_word(&s);
+    println!("The first word is: {}", word);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
-    // For now, know that iter is a method that returns each element in a
-    // collection and that enumerate wraps the result of iter and returns each
-    // element as part of a tuple instead. This is a bit more convenient than
-    // calculating the index ourselves
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
