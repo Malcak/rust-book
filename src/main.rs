2a4c305f8eb2c13f2d800bdd83a5b279c4a4af3d
Malcak <malcakx@gmail.com>
2022-01-25 22:09:35 -0500

    04-02-only-one-mutable-reference-at-time

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let mut s = String::from("hello");
 
-    change(&mut s);
-}
+    let r1 = &mut s;
+    let r2 = &mut s;
+    // cannot borrow `s` as mutable more than once at a time
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    println!("{}, {}", r1, r2);
 }
