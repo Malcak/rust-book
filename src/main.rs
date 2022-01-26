3676b57f65bde21ffb70af38d082ae68b40e321f
Malcak <malcakx@gmail.com>
2022-01-25 22:32:42 -0500

    04-02-dangling-references

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-// this code will compile because the last usage of the immutable references,
-// the println!, occurs before the mutable reference is introduced
-
 fn main() {
-    let mut s = String::from("hello");
-
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{} and {}", r1, r2);
-    // variables r1 and r2 will not be used after this point
-
-    let r3 = &mut s; // no problem
-    println!("{}", r3);
+    let reference_to_nothing = dangle();
 }
+
+fn dangle() -> &String { // dangle returns a reference to a String
+
+    let s = String::from("hello"); // s is a new String
+
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+  // Danger!
