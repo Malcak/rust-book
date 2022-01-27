4db6cb8a0279ff45e51874b02eaa04681c5189d6
Malcak <malcakx@gmail.com>
2022-01-26 23:02:32 -0500

    06-01-defining-an-enum

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,29 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+enum IpAddrKind {
+    V4,
+    V6,
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1")
     };
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1")
+    };
 }
+
+fn route(ip_kind: IpAddrKind) {}
