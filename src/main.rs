12888fcb5d0ab808e83af237ac5f4528590b4d87
Malcak <malcakx@gmail.com>
2022-01-26 20:26:19 -0500

    05-01-tuple-struct

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
 fn main() {
-    let user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
-    };
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
 
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
