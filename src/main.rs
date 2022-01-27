6f7eb3984f3d945c65530d1479ea48feace89fe1
Malcak <malcakx@gmail.com>
2022-01-26 20:30:11 -0500

    05-01-unit-like-structs-without-any-fields

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 fn main() {
-    struct Color(i32, i32, i32);
-    struct Point(i32, i32, i32);
+    struct AlwaysEqual;
 
-    let black = Color(0, 0, 0);
-    let origin = Point(0, 0, 0);
+    let subject = AlwaysEqual;
 }
