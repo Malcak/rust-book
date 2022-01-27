81e3a2618b55203f9598f23c91872786db462104
Malcak <malcakx@gmail.com>
2022-01-26 21:38:02 -0500

    05-03-method-syntax

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let scale = 2;
-    let rectangle = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
+    let rect = Rectangle {
+        width: 30,
+        height: 50
     };
 
-    dbg!(&rectangle);
+    println!("The area of the rectangle is {} square pixels.",rect.area())
 }
