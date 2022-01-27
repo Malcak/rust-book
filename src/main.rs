2a22d0e365d328d4a5b0625f7bda9614b8c032c6
Malcak <malcakx@gmail.com>
2022-01-26 20:40:45 -0500

    05-02-reactangle-with-single-variables

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-    struct AlwaysEqual;
+    let width = 30;
+    let height = 50;
 
-    let subject = AlwaysEqual;
+    println!("The area of the rectangle is {} square pixels.", area(width, height));
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
