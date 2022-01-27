bdf9e4a5f7a04aa97ec9abb9b1dc43ab6376d3c7
Malcak <malcakx@gmail.com>
2022-01-26 21:12:37 -0500

    05-02-print-a-structure-without-display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -9,9 +10,5 @@ fn main() {
         height: 50,
     };
 
-    println!("The area of the rectangle is {} square pixels.", area(&rectangle));
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("rectangle is {:#?}", &rectangle);
 }
