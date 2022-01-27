20ccd33d3687a3c8b78d81a07c140268880a608b
Malcak <malcakx@gmail.com>
2022-01-27 00:50:43 -0500

    06-02-match-control-flow

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-fn main() {
-    let x: i8 = 5;
-    let y: Option<i8> = Some(5);
-
-    // no implementation for `i8 + Option<i8>`
-    let sum = x + y;
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
 }
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {}
