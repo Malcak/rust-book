3977b442820b79cfe13963dc4a3f1f29df5c04e7
Malcak <malcakx@gmail.com>
2022-01-26 20:16:50 -0500

    05-01-struct-update-syntax

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,16 @@ struct User {
     sign_in_count: u64,
 }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
-    }
-}
+    };
 
-fn main() {
-    let user1 = build_user(
-        String::from("someone@example.com"),
-        String::from("someusername123"),
-    );
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
 }
